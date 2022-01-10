46743a366d86f2ff8d5ca6c2747a7781da82aded
kyleb <kyleb@BLD>
2022-01-10 17:32:11 -0500

    Add project files.

---
+++ BessSlotMachine/Form1.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace BessSlotMachine
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pbSlot1 = new System.Windows.Forms.PictureBox();
+            this.panelSlot1 = new System.Windows.Forms.Panel();
+            this.panelSlot2 = new System.Windows.Forms.Panel();
+            this.pbSlot2 = new System.Windows.Forms.PictureBox();
+            this.panelSlot3 = new System.Windows.Forms.Panel();
+            this.pbSlot3 = new System.Windows.Forms.PictureBox();
+            this.panelTitle = new System.Windows.Forms.Panel();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblInstructions = new System.Windows.Forms.Label();
+            this.pleaseGambleResponsibly = new System.Windows.Forms.Label();
+            this.btnSpin = new System.Windows.Forms.Button();
+            this.lblBet = new System.Windows.Forms.Label();
+            this.tbBet = new System.Windows.Forms.TextBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pbSlot1)).BeginInit();
+            this.panelSlot1.SuspendLayout();
+            this.panelSlot2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbSlot2)).BeginInit();
+            this.panelSlot3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbSlot3)).BeginInit();
+            this.panelTitle.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pbSlot1
+            // 
+            this.pbSlot1.Image = global::BessSlotMachine.Properties.Resources.Watermelon;
+            this.pbSlot1.Location = new System.Drawing.Point(9, 10);
+            this.pbSlot1.Name = "pbSlot1";
+            this.pbSlot1.Size = new System.Drawing.Size(159, 161);
+            this.pbSlot1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbSlot1.TabIndex = 0;
+            this.pbSlot1.TabStop = false;
+            // 
+            // panelSlot1
+            // 
+            this.panelSlot1.BackColor = System.Drawing.Color.Red;
+            this.panelSlot1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelSlot1.Controls.Add(this.pbSlot1);
+            this.panelSlot1.Location = new System.Drawing.Point(18, 169);
+            this.panelSlot1.Name = "panelSlot1";
+            this.panelSlot1.Size = new System.Drawing.Size(178, 182);
+            this.panelSlot1.TabIndex = 1;
+            // 
+            // panelSlot2
+            // 
+            this.panelSlot2.BackColor = System.Drawing.Color.Red;
+            this.panelSlot2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelSlot2.Controls.Add(this.pbSlot2);
+            this.panelSlot2.Location = new System.Drawing.Point(218, 169);
+            this.panelSlot2.Name = "panelSlot2";
+            this.panelSlot2.Size = new System.Drawing.Size(178, 182);
+            this.panelSlot2.TabIndex = 2;
+            // 
+            // pbSlot2
+            // 
+            this.pbSlot2.Image = global::BessSlotMachine.Properties.Resources.Watermelon;
+            this.pbSlot2.Location = new System.Drawing.Point(9, 10);
+            this.pbSlot2.Name = "pbSlot2";
+            this.pbSlot2.Size = new System.Drawing.Size(159, 161);
+            this.pbSlot2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbSlot2.TabIndex = 0;
+            this.pbSlot2.TabStop = false;
+            // 
+            // panelSlot3
+            // 
+            this.panelSlot3.BackColor = System.Drawing.Color.Red;
+            this.panelSlot3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelSlot3.Controls.Add(this.pbSlot3);
+            this.panelSlot3.Location = new System.Drawing.Point(419, 169);
+            this.panelSlot3.Name = "panelSlot3";
+            this.panelSlot3.Size = new System.Drawing.Size(178, 182);
+            this.panelSlot3.TabIndex = 2;
+            // 
+            // pbSlot3
+            // 
+            this.pbSlot3.Image = global::BessSlotMachine.Properties.Resources.Watermelon;
+            this.pbSlot3.Location = new System.Drawing.Point(9, 10);
+            this.pbSlot3.Name = "pbSlot3";
+            this.pbSlot3.Size = new System.Drawing.Size(159, 161);
+            this.pbSlot3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbSlot3.TabIndex = 0;
+            this.pbSlot3.TabStop = false;
+            // 
+            // panelTitle
+            // 
+            this.panelTitle.BackColor = System.Drawing.Color.LightGray;
+            this.panelTitle.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panelTitle.Controls.Add(this.lblTitle);
+            this.panelTitle.Location = new System.Drawing.Point(207, 12);
+            this.panelTitle.Name = "panelTitle";
+            this.panelTitle.Size = new System.Drawing.Size(200, 52);
+            this.panelTitle.TabIndex = 1;
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F);
+            this.lblTitle.Location = new System.Drawing.Point(-3, 2);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(203, 46);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "The Slotto";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.LightGray;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.lblInstructions);
+            this.panel1.Location = new System.Drawing.Point(18, 79);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(579, 81);
+            this.panel1.TabIndex = 3;
+            // 
+            // lblInstructions
+            // 
+            this.lblInstructions.AutoSize = true;
+            this.lblInstructions.Font = new System.Drawing.Font("Nirmala UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInstructions.Location = new System.Drawing.Point(5, 6);
+            this.lblInstructions.Name = "lblInstructions";
+            this.lblInstructions.Size = new System.Drawing.Size(556, 60);
+            this.lblInstructions.TabIndex = 4;
+            this.lblInstructions.Text = "Thank you for choosing The Slotto for your gambling needs! Enter the amount you \r" +
+    "\nwould like to bet and hit Spin! Please gamble responsibly.\r\n\r\n";
+            // 
+            // pleaseGambleResponsibly
+            // 
+            this.pleaseGambleResponsibly.AutoSize = true;
+            this.pleaseGambleResponsibly.Location = new System.Drawing.Point(12, 504);
+            this.pleaseGambleResponsibly.Name = "pleaseGambleResponsibly";
+            this.pleaseGambleResponsibly.Size = new System.Drawing.Size(138, 13);
+            this.pleaseGambleResponsibly.TabIndex = 4;
+            this.pleaseGambleResponsibly.Text = "Please Gamble Responsibly";
+            // 
+            // btnSpin
+            // 
+            this.btnSpin.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.btnSpin.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSpin.Location = new System.Drawing.Point(218, 357);
+            this.btnSpin.Name = "btnSpin";
+            this.btnSpin.Size = new System.Drawing.Size(178, 62);
+            this.btnSpin.TabIndex = 5;
+            this.btnSpin.Text = "Spin!";
+            this.btnSpin.UseVisualStyleBackColor = false;
+            this.btnSpin.Click += new System.EventHandler(this.btnSpin_Click);
+            // 
+            // lblBet
+            // 
+            this.lblBet.AutoSize = true;
+            this.lblBet.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBet.Location = new System.Drawing.Point(192, 428);
+            this.lblBet.Name = "lblBet";
+            this.lblBet.Size = new System.Drawing.Size(98, 20);
+            this.lblBet.TabIndex = 6;
+            this.lblBet.Text = "Bet Amount:";
+            // 
+            // tbBet
+            // 
+            this.tbBet.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.tbBet.Location = new System.Drawing.Point(296, 425);
+            this.tbBet.Name = "tbBet";
+            this.tbBet.Size = new System.Drawing.Size(100, 26);
+            this.tbBet.TabIndex = 7;
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.btnExit.Location = new System.Drawing.Point(429, 460);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(159, 57);
+            this.btnExit.TabIndex = 8;
+            this.btnExit.Text = "Cash Out";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnSpin;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkGray;
+            this.CancelButton = this.btnExit;
+            this.ClientSize = new System.Drawing.Size(609, 526);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.tbBet);
+            this.Controls.Add(this.lblBet);
+            this.Controls.Add(this.btnSpin);
+            this.Controls.Add(this.pleaseGambleResponsibly);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panelTitle);
+            this.Controls.Add(this.panelSlot3);
+            this.Controls.Add(this.panelSlot2);
+            this.Controls.Add(this.panelSlot1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pbSlot1)).EndInit();
+            this.panelSlot1.ResumeLayout(false);
+            this.panelSlot2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pbSlot2)).EndInit();
+            this.panelSlot3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pbSlot3)).EndInit();
+            this.panelTitle.ResumeLayout(false);
+            this.panelTitle.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbSlot1;
+        private System.Windows.Forms.Panel panelSlot1;
+        private System.Windows.Forms.Panel panelSlot2;
+        private System.Windows.Forms.PictureBox pbSlot2;
+        private System.Windows.Forms.Panel panelSlot3;
+        private System.Windows.Forms.PictureBox pbSlot3;
+        private System.Windows.Forms.Panel panelTitle;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblInstructions;
+        private System.Windows.Forms.Label pleaseGambleResponsibly;
+        private System.Windows.Forms.Button btnSpin;
+        private System.Windows.Forms.Label lblBet;
+        private System.Windows.Forms.TextBox tbBet;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ BessSlotMachine/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BessSlotMachine
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int img1, img2, img3;
+        double bet = 0;
+        double totalIn = 0;
+        double total = 0;
+       
+        Random numRand = new Random();
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            if(total > 0)
+            {
+                MessageBox.Show(string.Concat("You won ", total, " dollars! Total Bet: ", totalIn), "Winnings", MessageBoxButtons.OK);
+            }
+           else if(total == 0)
+            {
+                MessageBox.Show(string.Concat("You broke even. Total Bet: ", totalIn), "No Winnings", MessageBoxButtons.OK);
+            }
+           else
+            {
+                MessageBox.Show(string.Concat("You lost ", total * -1, " dollars. Total Bet: ", totalIn), "Better Luck Next Time", MessageBoxButtons.OK);
+            }
+            Close();
+        }
+
+        private void imageLoad(PictureBox pb, ref int num)
+        {
+            num = numRand.Next(0, 9);
+            switch (num){
+                case 0:
+                    pb.Load("../FruitSymbols/Apple.bmp");
+                    break;
+                case 1:
+                    pb.Load("../FruitSymbols/Banana.bmp");
+                    break;
+                case 2:
+                    pb.Load("../FruitSymbols/Cherries.bmp");
+                    break;
+                case 3:
+                    pb.Load("../FruitSymbols/Grapes.bmp");
+                    break;
+                case 4:
+                    pb.Load("../FruitSymbols/Lemon.bmp");
+                    break;
+                case 5:
+                    pb.Load("../FruitSymbols/Lime.bmp");
+                    break;
+                case 6:
+                    pb.Load("../FruitSymbols/Orange.bmp");
+                    break;
+                case 7:
+                    pb.Load("../FruitSymbols/Pear.bmp");
+                    break;
+                case 8:
+                    pb.Load("../FruitSymbols/Strawberry.bmp");
+                    break;
+                case 9:
+                    pb.Load("../FruitSymbols/Watermelon.bmp");
+                    break;
+
+            }
+
+        }
+
+
+
+        private void btnSpin_Click(object sender, EventArgs e)
+        {
+            if(double.TryParse(tbBet.Text, out bet))
+            {
+                if(bet > 0)
+                {
+                    total -= bet;
+                    totalIn += bet;
+                    imageLoad(pbSlot1, ref img1);
+                    imageLoad(pbSlot2, ref img2);
+                    imageLoad(pbSlot3, ref img3);
+                    if(img1 == img2 && img2 == img3)
+                    {
+                        total += bet * 3;
+                        MessageBox.Show(string.Concat("You won ", bet * 3, "$!"), "Winner", MessageBoxButtons.OK);
+                    }
+                    else if(img1 == img2 || img1 == img3 || img2 == img3)
+                    {
+                        total += bet * 2;
+                        MessageBox.Show(string.Concat("You won ", bet * 2, "$!"), "Winner", MessageBoxButtons.OK);
+                    }
+                    else
+                    {
+                        MessageBox.Show("You won 0$", "Try Again!", MessageBoxButtons.OK);
+                    }
+                }
+                else if(bet == 0)
+                {
+                    MessageBox.Show("Invalid Bet: This machine needs to make money too. Please bet more than 0.00$. Thank You", "This isnt a free game", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    MessageBox.Show("Invalid Bet: You can't flip the odds on us. Please enter an amount greater than 0.00$.", "The house should ALMOST always win", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Invalid Bet: Please enter a valid number of dollars to bet", "Bad Input", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+    }
+}
